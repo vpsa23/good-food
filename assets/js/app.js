d129124bb62aeba293e7173f86ead9bdc7c8d8e3
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-02-21 18:06:10 -0300

    agregando buscador

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,71 @@
 $(document).ready(() => {
+  //buscador 
+  inputSearch = $('#search');
+  inputSearch.click(function() {
+    let list = `
+    <div class="row">
+      <div class="col-sm-12">
+        <ul class="list-group">
+          <li class="label">Busquedas Recomendadas</li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_8.png?fit=around%7C88%3A88&amp;crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Desayuno</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_9.png?fit=around%7C88%3A88&amp;crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Almuerzo</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_10.png?fit=around%7C88%3A88&crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Cena</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_1.png?fit=around%7C88%3A88&crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Restaurantes con Delivery</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_3.png?fit=around%7C88%3A88&crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Tragos y Vida Nocturna</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_5.png?fit=around%7C88%3A88&crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Para llevar</span>
+          </li>
+          <li class="list-group-item">
+            <img class="imgLi" src="https://b.zmtcdn.com/images/search_tokens/app_icons/category_6.png?fit=around%7C88%3A88&crop=88%3A88%3B%2A%2C%2A" alt="" />
+            <span>Cafés</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+    `
+    $('#containerUL').append(list);
+  })
 
+
+// me trae las 13 categorias
+  $.ajax({
+    type: "GET", //it's a GET request API
+    headers: {
+      'X-Zomato-API-Key': '8149c00360df56f4399c2cf208281f7a' //'5e26f0851bb93b15813c1f2b7af9f77a'
+    },
+    url: 'https://developers.zomato.com/api/v2.1/categories',
+    dataType: 'json',
+    processData: true,
+    success: (categories) => {
+      console.log(categories);
+      
+    }
+  });
+
+
+
+
+
+
+
+
+//trae un restaurant en particular
 $.ajax({
     type: "GET", //it's a GET request API
     headers: {
@@ -59,8 +125,29 @@ $.ajax({
     }
   });
 
+  $.ajax({
+    type: "GET", //it's a GET request API
+    headers: {
+      'X-Zomato-API-Key': '8149c00360df56f4399c2cf208281f7a' //'5e26f0851bb93b15813c1f2b7af9f77a'
+    },
+    url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=280&lat=-36.85235938460808&lon=174.76295471191406=${data}',
+    dataType: 'json',
+    data: {
+      cuisine_id: '6',
+      //res_id: '16788789',
+      //res_id: '16793056',
+      //res_id: '16781429',
+    },
+    processData: true,
+    success: (data) => {
+      console.log(data);
 
+    }
+  });
 });
+
+
+
 
 
 
