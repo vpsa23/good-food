38f54541219f681d21b9cc9dc7df654b4003a29a
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-02-21 21:25:31 -0300

    modificando funciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,7 +43,7 @@ $(document).ready(() => {
   })
 
 
-// me trae las 13 categorias
+//traemos 6 categorias de la api
   $.ajax({
     type: "GET", //it's a GET request API
     headers: {
@@ -59,7 +59,7 @@ $(document).ready(() => {
           <div class="row">
             <div class="col-sm-4 col-md-4 col-lg-4">
               <h4>${categories[i][0].categories.name}</h4>
-              <img src="assets/img/delivery.jpg" alt="" />
+              <img class="images" src="assets/img/delivery.jpg" alt="" />
             </div>
             <div class="col-sm-4 col-md-4 col-lg-4">
               <h4>${categories[i][5].categories.name}</h4>
@@ -91,7 +91,101 @@ $(document).ready(() => {
   });
 
 
-
+//traemos los tipos de comida
+  $.ajax({
+    type: "GET", //it's a GET request API
+    headers: {
+      'X-Zomato-API-Key': '8149c00360df56f4399c2cf208281f7a' //'5e26f0851bb93b15813c1f2b7af9f77a'
+    },
+    url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=280&lat=-36.85235938460808&lon=174.76295471191406=${data}',
+    dataType: 'json',
+    processData: true,
+    success: (cuisines) => {
+      console.log(cuisines);
+      for (let i in cuisines) {
+        //let italian = cuisines[i][67].cuisine.cuisine_name;
+        //console.log(italian);
+//categories[i][8].categories.name
+        
+        let comidas = `
+          <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][2].cuisine.cuisine_name}</h4>
+              <img src="assets/img/american.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][4].cuisine.cuisine_name}</h4>
+              <img src="assets/img/argentina.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][6].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][9].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][12].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][16].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][67].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][15].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][19].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][21].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][29].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][31].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][40].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][43].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][48].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+            <div class="col-sm-3 col-md-3 col-lg-3">
+              <h4>${cuisines[i][61].cuisine.cuisine_name}</h4>
+              <img src="assets/img/delivery.jpg" alt="" />
+            </div>
+          </div>
+        `;
+        console.log(comidas);
+        $('#containerCuisines').append(comidas);
+      }
+    }
+  });
 
 
 
@@ -157,25 +251,6 @@ $.ajax({
     }
   });
 
-  $.ajax({
-    type: "GET", //it's a GET request API
-    headers: {
-      'X-Zomato-API-Key': '8149c00360df56f4399c2cf208281f7a' //'5e26f0851bb93b15813c1f2b7af9f77a'
-    },
-    url: 'https://developers.zomato.com/api/v2.1/cuisines?city_id=280&lat=-36.85235938460808&lon=174.76295471191406=${data}',
-    dataType: 'json',
-    data: {
-      cuisine_id: '6',
-      //res_id: '16788789',
-      //res_id: '16793056',
-      //res_id: '16781429',
-    },
-    processData: true,
-    success: (data) => {
-      console.log(data);
-
-    }
-  });
 });
 
 
